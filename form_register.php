fd041c424ac1dcee280453789c083be6f33a7abf
Serghei Muntean <sergiu1607@gmail.com>
2019-10-31 20:10:00 +0200

    varsion_article_2: pole-povtorite-parol

--- form_register.php
+++ form_register.php
@@ -36,25 +36,48 @@
 
         //================ Проверка длины пароля ==================
         var password = $('input[name=password]');
+        var confirm_password = $('input[name=confirm_password]');
 
         password.blur(function(){
-            if(password.val() != ''){
+            if(password.val() !== ''){
 
-                //Если длина введенного пароля меньше шести символов, то выводим сообщение об ошибке
+                //Если длина введённого пароля меньше шести символов, то выводим сообщение об ошибке
                 if(password.val().length < 6){
                     //Выводим сообщение об ошибке
                     $('#valid_password_message').text('Минимальная длина пароля 6 символов');
+
+                    //проверяем, если пароли не совпадают, то выводим сообщение об ошибке
+                    if(password.val() !== confirm_password.val()){
+                        debugger
+                        //Выводим сообщение об ошибке
+                        $('#valid_confirm_password_message').text('Пароли не совпадают');
+                    }else{
+                        $('#valid_confirm_password_message').empty();
+                    }
 
                     // Дезактивируем кнопку отправки
                     $('input[type=submit]').attr('disabled', true);
 
                 }else{
-                    // Убираем сообщение об ошибке
-                    $('#valid_password_message').text('');
+                    //Иначе, если длина первого пароля больше шести символов, то мы также проверяем, если они  совпадают.
+                    if(password.val() !== confirm_password.val()){
+                        //Выводим сообщение об ошибке
+                        $('#valid_confirm_password_message').text('Пароли не совпадают');
 
-                    //Активируем кнопку отправки
-                    $('input[type=submit]').attr('disabled', false);
+                        // Дезактивируем кнопку отправки
+                        $('input[type=submit]').attr('disabled', true);
+                    }else{
+                        // Убираем сообщение об ошибке у поля для ввода повторного пароля
+                        $('#valid_confirm_password_message').text('');
+
+                        //Активируем кнопку отправки
+                        $('input[type=submit]').attr('disabled', false);
+                    }
+
+                    // Убираем сообщение об ошибке у поля для ввода пароля
+                    $('#valid_password_message').text('');
                 }
+
             }else{
                 $('#valid_password_message').text('Введите пароль');
             }
