d8635babe030e7d53296415905ceef2743937afa
Serghei Muntean <sergiu1607@gmail.com>
2019-11-20 21:03:50 +0200

    smtp ssl

--- register.php
+++ register.php
@@ -295,13 +295,12 @@
                 $mail->isSMTP(); // Указываем что необходимо использовать SMTP
 
                 $mail->Host = 'smtp.gmail.com'; // Указываем SMTP сервер, который будет отправлять письма
-
-                $mail->Port = 587; // 587 - TLS; 465 - SSL; TCP порт. Этот порт может отличаться у других провайдеров
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // PHPMailer::ENCRYPTION_STARTTLS;, PHPMailer::ENCRYPTION_SMTPS;
-
                 $mail->SMTPAuth = true; // Включаем SMTP авторизацию
                 $mail->Username = $username_smtp; // Указан в файле dbconnect.php. Ваш логин от почты с которой будут отправляться письма
                 $mail->Password = $password_smtp; // Указан в файле dbconnect.php. Ваш пароль от почты с которой будут отправляться письма
+
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // PHPMailer::ENCRYPTION_STARTTLS;, PHPMailer::ENCRYPTION_SMTPS;
+                $mail->Port = 465; // 587 - TLS; 465 - SSL; TCP порт. Этот порт может отличаться у других провайдеров
                 
                 $mail->setFrom($username_smtp); // от кого будет уходить письмо?
 
