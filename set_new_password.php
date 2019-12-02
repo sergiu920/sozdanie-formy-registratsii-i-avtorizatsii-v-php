e081b3191ec2660dad8560a4b2947b2366219226
Serghei Muntean <sergiu1607@gmail.com>
2019-12-02 21:47:31 +0200

    ustanovka-novogo-parolya

--- set_new_password.php
+++ set_new_password.php
@@ -29,7 +29,110 @@ if($query_select_user->num_rows == 1){
         if($token == $row['reset_password_token']){
 
             //(1) Место для вывода формы установки нового пароля
+            //Подключение шапки
+            require_once("header.php");
+?>
+            <!-- Код JavaScript -->
+            <script type="text/javascript">
+                $(document).ready(function(){
+                    "use strict";
+                    //================ Прооверка паролей ==================
+                    var password = $('input[name=password]');
+                    var confirm_password = $('input[name=confirm_password]');
 
+                    password.blur(function(){
+                        if(password.val() != ''){
+                            //Если длина введённого пароля меньше шести символов, то выводим сообщение об ошибке
+                            if(password.val().length < 6){
+                                //Выводим сообщение об ошибке
+                                $('#valid_password_message').text('Минимальная длина пароля 6 символов');
+                                //проверяем, если пароли не совпадают, то выводим сообщение об ошибке
+                                if(password.val() !== confirm_password.val()){
+                                    //Выводим сообщение об ошибке
+                                    $('#valid_confirm_password_message').text('Пароли не совпадают');
+                                }
+                                // Дезактивируем кнопку отправки
+                                $('input[type=submit]').attr('disabled', true);
+
+                            }else{
+                                //Иначе, если длина первого пароля больше шести символов, то мы также проверяем, если они  совпадают.
+                                if(password.val() !== confirm_password.val()){
+                                    //Выводим сообщение об ошибке
+                                    $('#valid_confirm_password_message').text('Пароли не совпадают');
+                                    // Дезактивируем кнопку отправки
+                                    $('input[type=submit]').attr('disabled', true);
+                                }else{
+                                    // Убираем сообщение об ошибке у поля для ввода повторного пароля
+                                    $('#valid_confirm_password_message').text('');
+                                    //Активируем кнопку отправки
+                                    $('input[type=submit]').attr('disabled', false);
+                                }
+                                // Убираем сообщение об ошибке у поля для ввода пароля
+                                $('#valid_password_message').text('');
+                            }
+                        }else{
+                            $('#valid_password_message').text('Введите пароль');
+                        }
+                    });
+
+                    confirm_password.blur(function(){
+                        //Если пароли не совпадают
+                        if(password.val() !== confirm_password.val()){
+                            //Выводим сообщение об ошибке
+                            $('#valid_confirm_password_message').text('Пароли не совпадают');
+                            // Дезактивируем кнопку отправки
+                            $('input[type=submit]').attr('disabled', true);
+                        }else{
+                            //Иначе, проверяем длину пароля
+                            if(password.val().length > 6){
+                                // Убираем сообщение об ошибке у поля для ввода пароля
+                                $('#valid_password_message').text('');
+                                //Активируем кнопку отправки
+                                $('input[type=submit]').attr('disabled', false);
+                            }
+                            // Убираем сообщение об ошибке у поля для ввода повторного пароля
+                            $('#valid_confirm_password_message').text('');
+                        }
+                    });
+                });
+            </script>
+
+
+            <div class="center_block">
+
+                <h2>Установка нового пароля</h2>
+
+                <!-- Форма установки нового пароля -->
+                <form action="update_password.php" method="post">
+                    <table>
+                        <tr>
+                            <td> Введите новый пароль: </td>
+                            <td>
+                                <input type="password" name="password" placeholder="минимум 6 символов" required="required" /><br />
+                                <span id="valid_password_message" class="mesage_error"></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td> Повторите пароль: </td>
+                            <td>
+                                <input type="password" name="confirm_password" placeholder="минимум 6 символов" required="required" /><br />
+                                <span id="valid_confirm_password_message" class="mesage_error"></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <input type="hidden" name="token" value="<?=$token?>">
+                                <input type="hidden" name="email" value="<?=$email?>">
+                                <input type="submit" name="set_new_password" value="Изменить пароль" />
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+            
+<?php
+            //Подключение подвала
+            require_once("footer.php");
         }else{
             exit("<p><strong>Ошибка!</strong> Неправильный проверочный код.</p>");
         }
@@ -38,7 +141,7 @@ if($query_select_user->num_rows == 1){
         //Иначе, если есть ошибки в запросе к БД
         exit("<p><strong>Ошибка!</strong> Сбой при выборе пользователя из БД. </p>");
     }
-    
+
 }else{
     exit("<p><strong>Ошибка!</strong> Такой пользователь не зарегистрирован </p>");
 }
