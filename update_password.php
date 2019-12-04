419452b0a2329e03cbc0d2ae8429c8fabe0699a1
Serghei Muntean <sergiu1607@gmail.com>
2019-12-04 06:45:47 +0200

    obrabotka-formy-ustanovki-novogo-parolya

---
+++ update_password.php
@@ -0,0 +1,103 @@
+<?php
+
+//Запускаем сессию
+session_start();
+
+//Добавляем файл подключения к БД
+require_once("dbconnect.php");
+
+if(isset($_POST["set_new_password"]) && !empty($_POST["set_new_password"])){
+
+    //(1) Место для следующего куска кода
+
+    //Проверяем, если существует переменная token в глобальном массиве POST
+    if(isset($_POST['token']) && !empty($_POST['token'])){
+
+        $token = $_POST['token'];
+    }else{
+
+        //Останавливаем  скрипт
+        exit("<p><strong>Ошибка!</strong> Отсутствует проверочный код ( Передаётся скрытно ).</p>");
+    }
+
+    //Проверяем, если существует переменная email в глобальном массиве POST
+    if(isset($_POST['email']) && !empty($_POST['email'])){
+
+        $email = $_POST['email'];
+    }else{
+
+        //Останавливаем  скрипт
+        exit("<p><strong>Ошибка!</strong> Отсутствует адрес электронной почты ( Передаётся скрытно ).</p>");
+    }
+
+    if(isset($_POST["password"])){
+
+        //Обрезаем пробелы с начала и с конца строки
+        $password = trim($_POST["password"]);
+
+        //Проверяем, совпадают ли пароли
+        if(isset($_POST["confirm_password"])){
+
+            //Обрезаем пробелы с начала и с конца строки
+            $confirm_password = trim($_POST["confirm_password"]);
+
+            if($confirm_password != $password){
+                // Сохраняем в сессию сообщение об ошибке.
+                $_SESSION["error_messages"] = "<p class='mesage_error' >Пароли не совпадают</p>";
+
+                //Возвращаем пользователя на страницу установки нового пароля
+                header("HTTP/1.1 301 Moved Permanently");
+                header("Location: ".$address_site."set_new_password.php?email=$email&token=$token");
+
+                //Останавливаем  скрипт
+                exit();
+            }
+        }else{
+            // Сохраняем в сессию сообщение об ошибке.
+            $_SESSION["error_messages"] = "<p class='mesage_error' >Отсутствует поле для повторения пароля</p>";
+
+            //Возвращаем пользователя на страницу установки нового пароля
+            header("HTTP/1.1 301 Moved Permanently");
+            header("Location: ".$address_site."set_new_password.php?email=$email&token=$token");
+
+            //Останавливаем  скрипт
+            exit();
+        }
+
+        if(!empty($password)){
+
+            $password = htmlspecialchars($password, ENT_QUOTES);
+
+            //Шифруем папроль
+            $password = md5($password."top_secret");
+
+        }else{
+
+            // Сохраняем в сессию сообщение об ошибке.
+            $_SESSION["error_messages"] = "<p class='mesage_error' >Пароль не может быть пустым</p>";
+
+            //Возвращаем пользователя на страницу установки нового пароля
+            header("HTTP/1.1 301 Moved Permanently");
+            header("Location: ".$address_site."set_new_password.php?email=$email&token=$token");
+            //Останавливаем  скрипт
+            exit();
+        }
+
+    }else{
+        // Сохраняем в сессию сообщение об ошибке.
+        $_SESSION["error_messages"] = "<p class='mesage_error' >Отсутствует поле для ввода пароля</p>";
+
+        //Возвращаем пользователя на страницу установки нового пароля
+        header("HTTP/1.1 301 Moved Permanently");
+        header("Location: ".$address_site."set_new_password.php?email=$email&token=$token");
+
+        //Останавливаем  скрипт
+        exit();
+    }
+
+    //(2) Место для следующего куска кода
+
+}else{
+    exit("<p><strong>Ошибка!</strong> Вы зашли на эту страницу напрямую, поэтому нет данных для обработки. Вы можете перейти на <a href=".$address_site."> главную страницу </a>.</p>");
+}
+?>
